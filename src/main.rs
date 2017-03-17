6f75f178abf7809d916775081cd1c887da3d269b
mistrpopo <fpegeot@gmail.com>
2017-03-17 16:28:01 +0900

    add yellow cyan and magenta bins

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,9 @@ fn main() {
     let mut count_r = 0;
     let mut count_g = 0;
     let mut count_b = 0;
+    let mut count_y = 0;
+    let mut count_m = 0;
+    let mut count_c = 0;
 
     let thresh = 10;
 
@@ -53,6 +56,15 @@ fn main() {
             if r >= (max_pix - thresh) && g >= (max_pix - thresh) && b >= (max_pix - thresh) {
                 count_gray += 1;
             }
+            else if r >= (max_pix - thresh) && g >= (max_pix - thresh) {
+                count_y += 1;
+            }
+            else if r >= (max_pix - thresh) && b >= (max_pix - thresh) {
+                count_m += 1;
+            }
+            else if b >= (max_pix - thresh) && g >= (max_pix - thresh) {
+                count_c += 1;
+            }
             else if r == max_pix {
                 count_r += 1;
             }
@@ -64,8 +76,13 @@ fn main() {
             }
         }
     }
-    println!("counted {} black pixels, {} gray pixels, {} white pixels, {} red pixels, {} green pixels, {} blue pixels", 
-        count_black, count_gray, count_white, count_r, count_g, count_b);
+    println!("counted \
+             {} black pixels, {} gray pixels, {} white pixels, \
+             {} yellow pixels, {} magenta pixels, {} cyan pixels, \
+             {} red pixels, {} green pixels, {} blue pixels", 
+        count_black, count_gray, count_white, 
+        count_y, count_m, count_c,
+        count_r, count_g, count_b);
 //    let ref mut fout = File::create(&Path::new("D:\\Documents\\GitHub\\image-test\\res\\abc.jpg")).unwrap();
 
     // Write the contents of this image to the Writer in PNG format.
